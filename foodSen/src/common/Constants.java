4499a267bb066a4338e84aa3d5c449efaac83cac
nathan2ya <nathan2yo@gmail.com>
2014-07-10 13:37:04 +0900

    급식시설 개선사례 create완료

--- foodSen/src/common/Constants.java
+++ foodSen/src/common/Constants.java
@@ -5,7 +5,7 @@ public class Constants
     public static final String COMMON_FILE_PATH = "E:\\git project\\foodSen\\foodSen\\WebContent\\Upload\\"; //서버 파일 절대 경로
     
     public static final String MENU2_INSPECTIONRESULT_FILE_PATH = "menu2\\inspectionResult\\"; //위생.안전성 검사결과 업로드 파일 경로
-    public static final String MENU2_IMPROVEMENT_FILE_PATH = "menu2\\improvementCase\\"; //급식시설 개선 사례 업로드 파일 경로
+    public static final String MENU3_IMPROVEMENT_FILE_PATH = "menu3\\improvementCase\\"; //급식시설 개선 사례 업로드 파일 경로
     
 }
 
