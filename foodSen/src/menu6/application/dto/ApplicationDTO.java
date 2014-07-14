365706b9b9251943c0eb2a414313ccf748598f76
nathan2ya <nathan2yo@gmail.com>
2014-07-14 11:14:09 +0900

    recruit, application DAO, DTO

--- foodSen/src/menu6/application/dto/ApplicationDTO.java
+++ foodSen/src/menu6/application/dto/ApplicationDTO.java
@@ -1,5 +1,149 @@
 package menu6.application.dto;
 
-public class ApplicationDTO {
+import java.util.Date;
 
+public class ApplicationDTO {
+	 
+    private int seq;
+    private String title;
+    private String pw;
+    private String loc_seq; // 서울/대전/강원/대구/부산/전주
+    private String sex;
+    private String job; // 영양교사/영양사/조리사/조리원
+    private String gubun; // 전일제/시간제
+    private String email;
+    private String phone;
+    private String description;
+    private String attach_name;
+    private String attach_path;
+    private int hits;
+    private String writer;
+    private String reg_name;
+    private Date reg_date;
+    private String udt_name;
+    private Date udt_date;
+    private String age;
+    private String end_yn;
+    
+    
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getLoc_seq() {
+		return loc_seq;
+	}
+	public void setLoc_seq(String loc_seq) {
+		this.loc_seq = loc_seq;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public String getGubun() {
+		return gubun;
+	}
+	public void setGubun(String gubun) {
+		this.gubun = gubun;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getAttach_name() {
+		return attach_name;
+	}
+	public void setAttach_name(String attach_name) {
+		this.attach_name = attach_name;
+	}
+	public String getAttach_path() {
+		return attach_path;
+	}
+	public void setAttach_path(String attach_path) {
+		this.attach_path = attach_path;
+	}
+	public int getHits() {
+		return hits;
+	}
+	public void setHits(int hits) {
+		this.hits = hits;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getReg_name() {
+		return reg_name;
+	}
+	public void setReg_name(String reg_name) {
+		this.reg_name = reg_name;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public String getUdt_name() {
+		return udt_name;
+	}
+	public void setUdt_name(String udt_name) {
+		this.udt_name = udt_name;
+	}
+	public Date getUdt_date() {
+		return udt_date;
+	}
+	public void setUdt_date(Date udt_date) {
+		this.udt_date = udt_date;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getEnd_yn() {
+		return end_yn;
+	}
+	public void setEnd_yn(String end_yn) {
+		this.end_yn = end_yn;
+	}
 }
--- foodSen/src/menu6/recruit/dto/RecruitDTO.java
+++ foodSen/src/menu6/recruit/dto/RecruitDTO.java
@@ -1,5 +1,156 @@
 package menu6.recruit.dto;
 
-public class RecruitDTO {
+import java.util.Date;
 
+public class RecruitDTO {
+	
+    private int seq;
+    private String title;
+    private String pw;
+    private String school_name;
+    private String school_type; // 초/중/고
+    private String loc_seq; // 서울/대전/강원/대구/부산/전주
+    private String job; // 영양교사/영양사/조리사/조리원
+    private String gubun; // 전일제/시간제
+    private String email;
+    private String phone;
+    private String description;
+    private String attach_name;
+    private String attach_path;
+    private int hits;
+    private String writer;
+    private String reg_name;
+    private Date reg_date;
+	private String udt_name;
+	private Date udt_date;
+	private String end_yn;
+	private int rownum;
+	
+	
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getSchool_name() {
+		return school_name;
+	}
+	public void setSchool_name(String school_name) {
+		this.school_name = school_name;
+	}
+	public String getSchool_type() {
+		return school_type;
+	}
+	public void setSchool_type(String school_type) {
+		this.school_type = school_type;
+	}
+	public String getLoc_seq() {
+		return loc_seq;
+	}
+	public void setLoc_seq(String loc_seq) {
+		this.loc_seq = loc_seq;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public String getGubun() {
+		return gubun;
+	}
+	public void setGubun(String gubun) {
+		this.gubun = gubun;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getAttach_name() {
+		return attach_name;
+	}
+	public void setAttach_name(String attach_name) {
+		this.attach_name = attach_name;
+	}
+	public String getAttach_path() {
+		return attach_path;
+	}
+	public void setAttach_path(String attach_path) {
+		this.attach_path = attach_path;
+	}
+	public int getHits() {
+		return hits;
+	}
+	public void setHits(int hits) {
+		this.hits = hits;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getReg_name() {
+		return reg_name;
+	}
+	public void setReg_name(String reg_name) {
+		this.reg_name = reg_name;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public String getUdt_name() {
+		return udt_name;
+	}
+	public void setUdt_name(String udt_name) {
+		this.udt_name = udt_name;
+	}
+	public Date getUdt_date() {
+		return udt_date;
+	}
+	public void setUdt_date(Date udt_date) {
+		this.udt_date = udt_date;
+	}
+	public String getEnd_yn() {
+		return end_yn;
+	}
+	public void setEnd_yn(String end_yn) {
+		this.end_yn = end_yn;
+	}
+	public int getRownum() {
+		return rownum;
+	}
+	public void setRownum(int rownum) {
+		this.rownum = rownum;
+	}
 }
