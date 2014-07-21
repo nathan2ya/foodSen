fd18955bc6d132866a971fa3e4f7e913b698a60e
nathan2ya <nathan2yo@gmail.com>
2014-07-21 17:02:39 +0900

    연수행사 DTO, DAO(기본) 완성

--- foodSen/src/menu7/trainingEvent/dto/TrainingEventDTO.java
+++ foodSen/src/menu7/trainingEvent/dto/TrainingEventDTO.java
@@ -1,5 +1,150 @@
 package menu7.trainingEvent.dto;
 
+import java.util.Date;
+
 public class TrainingEventDTO {
+	
+	private int seq;
+	private String title;
+	private String gubun;
+	private String str_date;
+	private String end_date;
+	private String attach_name;
+	private String attach_path;
+	private String pw;
+	private String img1;
+	private String img2;
+	private String img3;
+	private String description;
+	private String up_seq;
+	private String hits;
+	private String writer;
+	private String reg_name;
+	private Date reg_date;
+	private String udt_name;
+	private Date udt_date;
+	private String turn;
+	
+	
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getGubun() {
+		return gubun;
+	}
+	public void setGubun(String gubun) {
+		this.gubun = gubun;
+	}
+	public String getStr_date() {
+		return str_date;
+	}
+	public void setStr_date(String str_date) {
+		this.str_date = str_date;
+	}
+	public String getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(String end_date) {
+		this.end_date = end_date;
+	}
+	public String getAttach_name() {
+		return attach_name;
+	}
+	public void setAttach_name(String attach_name) {
+		this.attach_name = attach_name;
+	}
+	public String getAttach_path() {
+		return attach_path;
+	}
+	public void setAttach_path(String attach_path) {
+		this.attach_path = attach_path;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getImg1() {
+		return img1;
+	}
+	public void setImg1(String img1) {
+		this.img1 = img1;
+	}
+	public String getImg2() {
+		return img2;
+	}
+	public void setImg2(String img2) {
+		this.img2 = img2;
+	}
+	public String getImg3() {
+		return img3;
+	}
+	public void setImg3(String img3) {
+		this.img3 = img3;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getUp_seq() {
+		return up_seq;
+	}
+	public void setUp_seq(String up_seq) {
+		this.up_seq = up_seq;
+	}
+	public String getHits() {
+		return hits;
+	}
+	public void setHits(String hits) {
+		this.hits = hits;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getReg_name() {
+		return reg_name;
+	}
+	public void setReg_name(String reg_name) {
+		this.reg_name = reg_name;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public String getUdt_name() {
+		return udt_name;
+	}
+	public void setUdt_name(String udt_name) {
+		this.udt_name = udt_name;
+	}
+	public Date getUdt_date() {
+		return udt_date;
+	}
+	public void setUdt_date(Date udt_date) {
+		this.udt_date = udt_date;
+	}
+	public String getTurn() {
+		return turn;
+	}
+	public void setTurn(String turn) {
+		this.turn = turn;
+	}
 
 }
