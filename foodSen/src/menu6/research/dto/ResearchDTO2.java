db1e58f3dcae6ca62d001889f6342f245856fe89
nathan2ya <nathan2yo@gmail.com>
2014-08-06 10:49:31 +0900

    설문조사 4개의 DAO DTO 구축완료

--- foodSen/src/menu6/research/dto/ResearchDTO2.java
+++ foodSen/src/menu6/research/dto/ResearchDTO2.java
@@ -1,5 +1,139 @@
 package menu6.research.dto;
 
-public class ResearchDTO2 {
+import java.util.Date;
 
+/*
+ * 작성자: 류재욱
+ * 설  명: 설문조사(문항) DTO
+ * 용  도: 
+*/
+
+public class ResearchDTO2 {
+	private int suri_seq;//문항번호
+	private int surq_seq;//설문번호
+	private int sur_seq;//문제번호
+	private String suri_title1;//문항제목1
+	private String suri_title2;//문항제목2
+	private String suri_title3;//문항제목3
+	private String suri_title4;//문항제목4
+	private String suri_title5;//문항제목5
+	private String writer;//작성자
+	private String reg_name;//등록자
+	private Date reg_date;//등록일
+	private String udt_name;//수정자
+	private Date udt_date;//수정일
+	private String suri_num1;//문항1
+	private String suri_num2;//문항2
+	private String suri_num3;//문항3
+	private String suri_num4;//문항4
+	private String suri_num5;//문항5
+	
+	public int getSuri_seq() {
+		return suri_seq;
+	}
+	public void setSuri_seq(int suri_seq) {
+		this.suri_seq = suri_seq;
+	}
+	public int getSurq_seq() {
+		return surq_seq;
+	}
+	public void setSurq_seq(int surq_seq) {
+		this.surq_seq = surq_seq;
+	}
+	public int getSur_seq() {
+		return sur_seq;
+	}
+	public void setSur_seq(int sur_seq) {
+		this.sur_seq = sur_seq;
+	}
+	public String getSuri_title1() {
+		return suri_title1;
+	}
+	public void setSuri_title1(String suri_title1) {
+		this.suri_title1 = suri_title1;
+	}
+	public String getSuri_title2() {
+		return suri_title2;
+	}
+	public void setSuri_title2(String suri_title2) {
+		this.suri_title2 = suri_title2;
+	}
+	public String getSuri_title3() {
+		return suri_title3;
+	}
+	public void setSuri_title3(String suri_title3) {
+		this.suri_title3 = suri_title3;
+	}
+	public String getSuri_title4() {
+		return suri_title4;
+	}
+	public void setSuri_title4(String suri_title4) {
+		this.suri_title4 = suri_title4;
+	}
+	public String getSuri_title5() {
+		return suri_title5;
+	}
+	public void setSuri_title5(String suri_title5) {
+		this.suri_title5 = suri_title5;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getReg_name() {
+		return reg_name;
+	}
+	public void setReg_name(String reg_name) {
+		this.reg_name = reg_name;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public String getUdt_name() {
+		return udt_name;
+	}
+	public void setUdt_name(String udt_name) {
+		this.udt_name = udt_name;
+	}
+	public Date getUdt_date() {
+		return udt_date;
+	}
+	public void setUdt_date(Date udt_date) {
+		this.udt_date = udt_date;
+	}
+	public String getSuri_num1() {
+		return suri_num1;
+	}
+	public void setSuri_num1(String suri_num1) {
+		this.suri_num1 = suri_num1;
+	}
+	public String getSuri_num2() {
+		return suri_num2;
+	}
+	public void setSuri_num2(String suri_num2) {
+		this.suri_num2 = suri_num2;
+	}
+	public String getSuri_num3() {
+		return suri_num3;
+	}
+	public void setSuri_num3(String suri_num3) {
+		this.suri_num3 = suri_num3;
+	}
+	public String getSuri_num4() {
+		return suri_num4;
+	}
+	public void setSuri_num4(String suri_num4) {
+		this.suri_num4 = suri_num4;
+	}
+	public String getSuri_num5() {
+		return suri_num5;
+	}
+	public void setSuri_num5(String suri_num5) {
+		this.suri_num5 = suri_num5;
+	}
 }
--- foodSen/src/menu6/research/dto/ResearchDTO3.java
+++ foodSen/src/menu6/research/dto/ResearchDTO3.java
@@ -1,5 +1,91 @@
 package menu6.research.dto;
 
-public class ResearchDTO3 {
+import java.util.Date;
 
+public class ResearchDTO3 {
+	private int surr_seq;//설문결과번호
+	private int suri_seq;//설문번호
+	private int surq_seq;//문제번호
+	private int sur_seq;//문항번호
+	private String suri_num;//선택문항
+	private String description;//선택사유
+	private String writer;//작성자
+	private String reg_name;//등록자
+	private Date reg_date;//등록일
+	private String udt_name;//수정자
+	private Date udt_date;//수정일
+	private String surq_title;//문제
+	
+	public int getSurr_seq() {
+		return surr_seq;
+	}
+	public void setSurr_seq(int surr_seq) {
+		this.surr_seq = surr_seq;
+	}
+	public int getSuri_seq() {
+		return suri_seq;
+	}
+	public void setSuri_seq(int suri_seq) {
+		this.suri_seq = suri_seq;
+	}
+	public int getSurq_seq() {
+		return surq_seq;
+	}
+	public void setSurq_seq(int surq_seq) {
+		this.surq_seq = surq_seq;
+	}
+	public int getSur_seq() {
+		return sur_seq;
+	}
+	public void setSur_seq(int sur_seq) {
+		this.sur_seq = sur_seq;
+	}
+	public String getSuri_num() {
+		return suri_num;
+	}
+	public void setSuri_num(String suri_num) {
+		this.suri_num = suri_num;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getReg_name() {
+		return reg_name;
+	}
+	public void setReg_name(String reg_name) {
+		this.reg_name = reg_name;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public String getUdt_name() {
+		return udt_name;
+	}
+	public void setUdt_name(String udt_name) {
+		this.udt_name = udt_name;
+	}
+	public Date getUdt_date() {
+		return udt_date;
+	}
+	public void setUdt_date(Date udt_date) {
+		this.udt_date = udt_date;
+	}
+	public String getSurq_title() {
+		return surq_title;
+	}
+	public void setSurq_title(String surq_title) {
+		this.surq_title = surq_title;
+	}
 }
